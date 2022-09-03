8343a0fe09492c9a33e8b656d19981b921bbc344
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2022-09-03 09:38:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# -*-coding:utf8;-*-
-
 from loadAndroid import Load
 from chatbot import Chatbot
 from util import Voz
@@ -18,7 +16,7 @@ aprender = True
 
 def iniciar():
     if not modo_texto_ativo:
-        voz.fale(load.iniciar(infos))  # Verificando nome e primeiro acesso
+        voz.fale(load.iniciar(infos)) 
     else:
         load.iniciar(infos)
 
@@ -40,9 +38,9 @@ def responder(entrada):
     print(load.nomeUsu, ": ", entrada)
     print(load.nomeIa, ": ", saida[0])
 
-    log.reg(entrada, saida[0])  # Registra log
+    log.reg(entrada, saida[0])  
 
-    if not saida[1] and aprender:  # Cadastra uma resposta se não existir
+    if not saida[1] and aprender:  
         if not modo_texto_ativo:
             chatbor.aprender1(entrada)
         else:
@@ -50,12 +48,7 @@ def responder(entrada):
 
 
 iniciar()
-# A função inciar tem o objetivo de garantiro regsitro do usuário se esse for a primerira vez que o app está rodando
-# Se não for a primeira vez, faz uma saudação comum (bom dia, boa tarde....)
 
 while True:
     responder(receber_entrada())
-    # A resposta que o usuário receberá seja por voz ou texto deriva da entrada que o usuário fornecer
-    # O clico de escuta/ responde se mantém a menos que o usuário diga algum comando de saída como "sair",
-    # comandos como repodução de música que colocam o app em espera ou pare o emulador QPython manualmente.
-
+    
